e43ee18b42fc3fcee6f1ac3a5121708d07c572bc
Daen12 <sdy971213@gmail.com>
2023-02-04 22:06:35 +0900

    Feb 4th commit

---
+++ DailyTask/src/feb0204_revision/Dig.java
@@ -0,0 +1,18 @@
+package feb0204_revision;
+
+public class Dig {
+	String name;
+	int age;
+	
+	Dig(){
+		this("쫑");
+	}
+	
+	Dig(String name){
+		this.name = name;
+	}
+	
+	
+}
+
+ 
---
+++ DailyTask/src/feb0204_revision/test.java
@@ -0,0 +1,11 @@
+package feb0204_revision;
+
+import java.util.Arrays;
+
+public class test {
+	public static void main(String[] args) {
+		Dig d = new Dig("new name");
+		
+		System.out.println(d.name);
+	}
+}
