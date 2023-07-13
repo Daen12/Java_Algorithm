58c14d82e1de5628b419baf64f4ca38246882d4a
Daen12 <111489407+Daen12@users.noreply.github.com>
2023-07-14 08:16:29 +0900

    0714

--- Baekjoon/Baekjoon_vscode/Q6118_숨바꼭질_silver1.java
+++ Baekjoon/Baekjoon_vscode/Q6118_숨바꼭질_silver1.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -7,10 +9,13 @@ public class Q6118_숨바꼭질_silver1 {
     // 다익스트라
     // 그래프 -> BFS
     static ArrayList<Integer>[] graph;
+    static int N;
+    static ArrayList<Integer> list;
+    static int maxLevel;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();// 6 헛간 개수
+        N = in.nextInt();// 6 헛간 개수
         int M = in.nextInt();// 7 이어진 수
         graph = new ArrayList[N + 1];
         for (int i = 0; i < N + 1; i++) {
@@ -23,16 +28,38 @@ public class Q6118_숨바꼭질_silver1 {
             graph[ed].add(st);
         }
         DFS();
+        // Collections.sort(list, (Integer i1, Integer i2) -> {
+        // return i1 - i2;
+        // });
+        System.out.println(maxLevel + ' ' + list.size());
     }
 
     public static void DFS() {
+        boolean[] visited = new boolean[N + 1];
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[] { 1, 0 });
-        int maxLevel = -1;
-        List<Integer> list = new ArrayList<>();
-
+        visited[1] = true;
+        maxLevel = -1;
+        list = new ArrayList<>();
+        int cnt = 0;
         while (!queue.isEmpty()) {
-
+            System.out.println("Poll");
+            int[] pa = queue.poll();
+            for (int child : graph[pa[0]]) {
+                if (!visited[child]) {
+                    queue.offer(new int[] { child, pa[1] + 1 });
+                    visited[child] = true;
+                    if (maxLevel < pa[1] + 1) {
+                        cnt = 1;
+                        maxLevel = pa[1] + 1;
+                        list = new ArrayList<>();
+                        list.add(child);
+                    } else if (maxLevel == pa[1] + 1) {
+                        cnt++;
+                        list.add(child);
+                    }
+                }
+            }
         }
     }
 }
